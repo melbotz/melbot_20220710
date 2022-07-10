d00bb908a498a12269ceee241a6b96b8fc2d52de
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-10 12:44:00 +0200

    gitlab pages

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-git push github
+# git push github
+git remote | xargs -L1 git push --all
 npm publish
 
 #RELAY="ws://nostr.rocks:1617"
 RELAY=$(git config gitmark.relay)
 GENESIS="gitmark:bdc0528dd88bd0dac229e62f5cf92064e1cd6705f5f0bcce26bfbe4ebcf122a8:0"
-CREATED=1657316029
+CREATED=1657404285
 
 
 git-mark-list $(git config gitmark.secret) --genesis "${GENESIS}" --created "${CREATED}" -g $(cat package.json | jq -r '.repository.url')  | websocat $RELAY
